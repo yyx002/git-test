7fa03a5af529c666653291524547dc06f7c75cd7
yyx <yyx@atguigu.com>
2022-04-25 16:38:08 +0800

    first commit

---
+++ src/main/java/com/yyx/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.yyx.git;
+
+/**
+ * @author Mr.Yang
+ * @description 功能描述
+ * @create 2022/4/25 16:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
